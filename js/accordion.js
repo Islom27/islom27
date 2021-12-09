92d194469e290e07332170b2a1a4a9db63d3d472
Islom27 <79913236+Islom27@users.noreply.github.com>
2021-12-09 19:20:30 +0500

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -2,13 +2,13 @@ const accordionQuestion = document.querySelectorAll(".accordion_question");
 
 accordionQuestion.forEach(accordionQuestion => {
   accordionQuestion.addEventListener("click", event => {
-    
+    //This part of code is used for no more than one accordion to be opened
     const currentlyActiveAccordionQuestion = document.querySelector(".accordion_question.active");
     if(currentlyActiveAccordionQuestion && currentlyActiveAccordionQuestion!==accordionQuestion) {
       currentlyActiveAccordionQuestion.classList.toggle("active");
       currentlyActiveAccordionQuestion.nextElementSibling.style.maxHeight = 0;
     }
-
+    //Controls accordion closing and opening
     accordionQuestion.classList.toggle("active");
     const accordionAnswerHolder = accordionQuestion.nextElementSibling;
     if(accordionQuestion.classList.contains("active")) {
--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,14 @@
 let sliderText = document.getElementsByClassName("slider_text");
 let sliderPoint = document.getElementsByClassName("slider_point");
 let currentSlide = 0;
-
+//Getting elements
 document.getElementById("btn_next").addEventListener("click", () => {
     changeSlide(currentSlide + 1)
 });
 document.getElementById("btn_back").addEventListener("click", () => {
     changeSlide(currentSlide - 1)
 });
-
+//Sliding function
 function changeSlide(moveTo) {
     if (moveTo >= sliderText.length) {moveTo = 0;}
     if (moveTo < 0) {moveTo = sliderText.length - 1;}
@@ -20,7 +20,7 @@ function changeSlide(moveTo) {
     
     currentSlide = moveTo;
 }
-
+//Slider index
 document.querySelectorAll('.slider_point').forEach((bullet, bulletIndex) => {
     bullet.addEventListener('click', () => {
         if (currentSlide !== bulletIndex) {
